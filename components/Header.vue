9c9973bca113c58019452903116f49d27a43ac08
wanghaitao <15124505701@163.com>
2019-07-02 09:51:13 +0800

    feat: add login

--- components/Header.vue
+++ components/Header.vue
@@ -10,6 +10,35 @@
           {{ item.name }}
         </v-btn>
       </v-toolbar-items>
+      <v-menu left bottom offset-y >
+        <template v-slot:activator="{ on }">
+          <v-btn
+            icon
+            large
+            v-on="on"
+            style="width: 64px;height: 64px;"
+          >
+            <v-avatar size="48px" tile>
+              <img
+                src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+                alt="Vuetify"
+              >
+            </v-avatar>
+          </v-btn>
+        </template>
+        <v-list>
+          <template 
+            v-for="(item, index) in items"
+          >
+            <v-list-tile :key="'list' + index">
+              <v-list-tile-title style="min-width: 120px;text-align: center;cursor: pointer;">
+                {{ item.title }}
+              </v-list-tile-title>
+            </v-list-tile>
+            <v-divider v-if="index + 1 < items.length" :key="'divider' + index"></v-divider>
+          </template>
+        </v-list>
+      </v-menu>
     </v-toolbar>
     <v-navigation-drawer v-model="drawer" fixed temporary>
       <v-list class="pa-0">
@@ -56,6 +85,10 @@ export default {
         { path: '/tag', name: '标签', icon: 'label' },
         { path: '/archive', name: '归档', icon: 'archive' },
         { path: '/about', name: '关于', icon: 'question_answer' }
+      ],
+      items: [
+        { title: '登录', type: 'login' },
+        { title: '退出', type: 'logout' }
       ],
       drawer: null
     }
