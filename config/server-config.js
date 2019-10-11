4292cf14b9aee503b6318080e0c2861645528f20
wanghaitao <15124505701@163.com>
2019-10-11 23:39:07 +0800

    chore: prod

--- config/server-config.js
+++ config/server-config.js
@@ -2,7 +2,7 @@ const serveConfig = {}
 
 serveConfig.blogName = 'Blog'
 serveConfig.devBaseURL = 'http://localhost:3000'
-serveConfig.prodBaseURL = ''
+serveConfig.prodBaseURL = '/api/portal'
 serveConfig.devProxyURL = 'http://127.0.0.1:8080/'
 serveConfig.baseApi = '/api/portal'
 export default serveConfig
